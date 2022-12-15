c5fb3705695bd00b96bbb7a93e9b42efb81d6f5c
lkk223303 <kenthuang@bigobject.io>
2022-12-15 15:51:53 +0800

    use Logisticregression to train model on predicting red wine or not

--- mysql.pyspark-start-pyspark-session.py
+++ mysql.pyspark-start-pyspark-session.py
@@ -1,5 +1,9 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml import Pipeline
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
 
 # Set the jdbc url 
 jdbc_url = "jdbc:mysql://localhost:13306/TestDB"
@@ -21,7 +25,7 @@ tableDF = spark.read \
 
 # Show the data in the DataFrame
 # tableDF.show()
-
+#  loading the data from the MySQL table
 wine_df = spark.read.format("jdbc") \
     .option("url", jdbc_url) \
     .option("dbtable", "Wines") \
@@ -29,3 +33,34 @@ wine_df = spark.read.format("jdbc") \
     .option("password", "mysql") \
     .option("driver", "com.mysql.cj.jdbc.Driver") \
     .load()
+
+# split the data into traing and testing sets
+train_df , test_df  = wine_df.randomSplit([.6,.4],seed=12345)
+predictors = ["fixed_acidity","volatile_acidity","citric_acid","residual_sugar","chlorides","free_so2",
+              "total_so2","density","pH","sulphates","alcohol"]
+# assemble the data into vectors
+vec_assembler = VectorAssembler(inputCols=predictors,outputCol="features")
+vec_train_df = vec_assembler.transform(train_df)
+vec_train_df.select("features","is_red").show(10)
+
+lr = LogisticRegression(labelCol="is_red",featuresCol="features")
+# lr_model = lr.fit(vec_train_df)
+# vec_test_df = vec_assembler.transform(test_df)
+# predictions1 = lr_model.transform(vec_test_df)
+
+pipeline = Pipeline(stages=[vec_assembler,lr])
+pipeline_model = pipeline.fit(train_df)
+predictions = pipeline_model.transform(test_df)
+
+evaluator =  BinaryClassificationEvaluator(labelCol="is_red")
+eval_result = evaluator.evaluate(predictions)
+# eval_result1 = evaluator.evaluate(predictions1)
+print("show predictions")
+predictions.show()
+
+print("use the `select` method to display the predicted and true labels")
+predictions.select("prediction","is_red").show()
+
+print("show evaluation results")
+print(eval_result)
+
